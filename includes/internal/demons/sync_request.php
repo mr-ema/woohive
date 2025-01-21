166967c90f2b885d050e835c4bd7a14fef531b33
mr-ema <ema.a.v.c2017@gmail.com>
2025-01-21 18:56:51 -0300

    feat: agregar action para la sincronizacion de productos

--- includes/internal/demons/sync_request.php
+++ includes/internal/demons/sync_request.php
@@ -6,6 +6,7 @@ use WooHive\Config\Constants;
 use WooHive\Utils\Helpers;
 
 use WC_Product;
+use WP_Post;
 
 
 /** Prevenir el acceso directo al script. */
@@ -16,18 +17,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Sync_Request {
 
     public static function init(): void {
-        add_action( 'save_post_product', array( self::class, 'on_product_save' ), 10, 3 );
-        add_action( 'save_post_product_variation', array( self::class, 'on_product_variation_save' ), 10, 3 );
+        add_action( 'save_post_product', [ self::class, 'on_product_save' ], 10, 3 );
+        add_action( 'save_post_product_variation', [ self::class, 'on_product_variation_save' ], 10, 3 );
+
+        add_action( 'woocommerce_product_import_pre_insert_product_object', [ self::class, 'on_import_start' ], 10, 2);
+        add_action( 'woocommerce_product_import_inserted_product_object', [ self::class, 'on_import_finished' ], 10, 2 );
+
+        add_action( Constants::PLUGIN_SLUG . '_sync_product',  [ self::class, 'on_sync_product' ] );
     }
 
-    public static function on_product_save( int $post_id, \WP_Post $post, bool $update ): void {
-        if ( 'product' !== $post->post_type || 'publish' !== $post->post_status || get_transient( 'sync_in_progress_' . $post_id ) ) {
+    public static function on_sync_product( WC_Product $product ): void {
+        if ( $product && Helpers::should_sync( $product ) ) {
+            self::set_sync_in_progress( $product->get_id(), true );
+
+            if ( Helpers::is_primary_site() ) {
+                self::sync_to_secondary_sites_data( $product );
+            } elseif ( Helpers::is_secondary_site() ) {
+                self::sync_to_primary_site_data( $product );
+            }
+        }
+    }
+
+    public static function on_product_save( int $post_id, WP_Post $post, bool $update ): void {
+        $sync_in_process = self::is_sync_in_progress( $post_id ) || self::is_importing_in_progress( $post_id );
+        if ( $sync_in_process || 'product' !== $post->post_type || 'publish' !== $post->post_status ) {
             return;
         }
 
         $product = wc_get_product( $post_id );
         if ( $product && Helpers::should_sync( $product ) ) {
-            set_transient( 'sync_in_progress_' . $post_id, true, 9 );
+            self::set_sync_in_progress( $post_id, true );
 
             if ( Helpers::is_primary_site() ) {
                 self::sync_to_secondary_sites_data( $product );
@@ -37,15 +56,16 @@ class Sync_Request {
         }
     }
 
-    public static function on_product_variation_save( int $post_id, \WP_Post $post, bool $update ): void {
+    public static function on_product_variation_save( int $post_id, WP_Post $post, bool $update ): void {
         $product_id = get_post_field( 'post_parent', $post_id );
-        if ( ! $product_id || get_transient( 'sync_in_progress_' . $product_id ) ) {
+        $sync_in_process = self::is_sync_in_progress( $post_id ) || self::is_importing_in_progress( $post_id );
+        if ( ! $product_id || $sync_in_process ) {
             return;
         }
 
         $product = wc_get_product( $product_id );
         if ( $product && Helpers::should_sync( $product ) ) {
-            set_transient( 'sync_in_progress_' . $product_id, true, 9 );
+            self::set_sync_in_progress( $post_id, true );
 
             if ( Helpers::is_primary_site() ) {
                 self::sync_to_secondary_sites_data( $product );
@@ -53,6 +73,21 @@ class Sync_Request {
                 self::sync_to_primary_site_data( $product );
             }
         }
+    }
+
+    public static function on_import_start( WC_Product $product, array $_data ): void {
+        $post_id = $product->get_id();
+        $unused = $_data;
+
+        self::set_importing_in_progress( $post_id, true );
+    }
+
+    public static function on_import_finished( WC_Product $product, array $_data ): void {
+        $post_id = $product->get_id();
+        $unused = $_data;
+
+        self::set_importing_in_progress( $post_id, false );
+        do_action( Constants::PLUGIN_SLUG . '_sync_product', $product );
     }
 
     /**
@@ -128,4 +163,29 @@ class Sync_Request {
             )
         );
     }
+
+    public static function is_importing_in_progress( int $post_id ): bool {
+        return get_transient( Constants::PLUGIN_SLUG . '_importing_in_progress_' . $post_id);
+    }
+
+    public static function is_sync_in_progress( int $post_id ): bool {
+        return get_transient( Constants::PLUGIN_SLUG . '_sync_in_progress_' . $post_id );
+    }
+
+    public  static function set_sync_in_progress( int $post_id, bool $finished ): void {
+        if ( $finished ) {
+            delete_transient( Constants::PLUGIN_SLUG . '_sync_in_progress_' . $post_id );
+        }
+
+        set_transient( Constants::PLUGIN_SLUG . '_sync_in_progress_' . $post_id, true, 9 );
+    }
+
+    public static function set_importing_in_progress( int $post_id, bool $finished ): void {
+        if ( $finished ) {
+            delete_transient( Constants::PLUGIN_SLUG . '_importing_in_progress_' . $post_id );
+        }
+
+        set_transient( Constants::PLUGIN_SLUG . '_importing_in_progress_' . $post_id, true, 9 );
+    }
+
 }
